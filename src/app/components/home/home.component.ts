94686f50506803c2eaa2cb537426ad60d3a89ceb
angpao <angpao.kittimasak@gmail.com>
2019-08-11 08:59:26 +0700

    commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product'
-
+import { ProductService } from '../../services/product.service'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,9 +10,13 @@ export class HomeComponent implements OnInit {
 
   products: Product[]
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.productService.getAllProducts().subscribe((res: Product[]) => {
+      this.products = res
+      console.log(this.products)
+    })
   }
 
   deleteProduct(id) {
